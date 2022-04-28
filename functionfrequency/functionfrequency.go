b5dde7caf5430a909930b4001ebfab166ed1a74f
VeneLooool <timofej.gromb38@gmail.com>
2022-04-28 14:29:19 +0300

    4 task done

--- functionfrequency/functionfrequency.go
+++ functionfrequency/functionfrequency.go
@@ -1,5 +1,73 @@
 package functionfrequency
 
-func FunctionFrequency(gocode []byte) []string {
-	return []string{}
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"sort"
+)
+
+type Function struct {
+	name   string
+	amount int
 }
+
+type AllFunctions []Function
+
+func FunctionFrequency(goCode []byte) (ansOutput []string) {
+	if goCode == nil {
+		return nil
+	}
+
+	fileSet := token.NewFileSet()
+	inputFile, err := parser.ParseFile(fileSet, "input.go", goCode, 0)
+	if err != nil {
+		return nil
+	}
+
+	frequencies := inspectFileForFunctions(inputFile)
+
+	var functions AllFunctions
+	for key, value := range frequencies {
+		functions = append(functions, Function{name: key, amount: value})
+	}
+
+	sort.Sort(sort.Reverse(functions))
+
+	for i, point := range functions {
+		ansOutput = append(ansOutput, point.name)
+		if i == 2 {
+			break
+		}
+	}
+
+	return ansOutput
+}
+
+func inspectFileForFunctions(inputFile *ast.File) (frequencies map[string]int) {
+	frequencies = make(map[string]int)
+
+	ast.Inspect(inputFile, func(mainNode ast.Node) bool {
+		switch nodeExpr := mainNode.(type) {
+		case *ast.CallExpr:
+			switch exprFunc := nodeExpr.Fun.(type) {
+			case *ast.Ident:
+				nameFunc := exprFunc.Name
+				frequencies[nameFunc] = frequencies[nameFunc] + 1
+			case *ast.SelectorExpr:
+				switch nestedNodeExpr := exprFunc.X.(type) {
+				case *ast.Ident:
+					nameFunc := nestedNodeExpr.Name + "." + exprFunc.Sel.Name
+					frequencies[nameFunc] = frequencies[nameFunc] + 1
+				}
+			}
+		}
+		return true
+	})
+
+	return frequencies
+}
+
+func (a AllFunctions) Len() int           { return len(a) }
+func (a AllFunctions) Less(i, j int) bool { return a[i].amount < a[j].amount }
+func (a AllFunctions) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
--- missingnumbers/missingnumbers.go
+++ missingnumbers/missingnumbers.go
@@ -1,5 +1,66 @@
 package missingnumbers
 
-func Missing(numbers []int) []int {
-	return []int{}
+import "sort"
+
+type FinderMissing struct {
+	numbers        []int
+	output         []int
+	gotOne, gotTwo bool
+	sizeNum        int
+}
+
+func newFinderMissing(numbers []int) *FinderMissing {
+	return &FinderMissing{
+		numbers: numbers,
+		output:  nil,
+	}
+}
+
+func (finder *FinderMissing) checkArrayForMissing() {
+	finder.sizeNum = len(finder.numbers)
+	sort.Ints(finder.numbers)
+	for i := 0; i < finder.sizeNum-1; i++ {
+		if finder.numbers[i] != finder.numbers[i+1]-1 {
+			finder.output = append(finder.output, finder.numbers[i+1]-1)
+		}
+		if finder.numbers[i] == 1 || finder.numbers[i+1] == 1 {
+			finder.gotOne = true
+		}
+		if finder.numbers[i] == 2 || finder.numbers[i+1] == 2 {
+			finder.gotTwo = true
+		}
+	}
+}
+
+func (finder *FinderMissing) checkOutputAndCorrect() {
+	if len(finder.output) != 2 {
+		if !finder.gotOne {
+			finder.output = append(finder.output, 1)
+			finder.gotOne = true
+		}
+		if !finder.gotTwo {
+			finder.output = append(finder.output, 2)
+			finder.gotTwo = true
+		}
+		if len(finder.output) == 0 {
+			finder.output = []int{finder.numbers[finder.sizeNum-1] + 1, finder.numbers[finder.sizeNum-1] + 2}
+		}
+		if len(finder.output) == 1 {
+			finder.output = append(finder.output, finder.numbers[finder.sizeNum-1]+1)
+		}
+	}
+}
+
+func Missing(numbers []int) (output []int) {
+	if numbers == nil {
+		return nil
+	}
+	if len(numbers) == 0 {
+		return []int{1, 2}
+	}
+
+	finder := newFinderMissing(numbers)
+	finder.checkArrayForMissing()
+	finder.checkOutputAndCorrect()
+	return finder.output
 }
