798af4f7ddf30556abe9a8604cb8ea8d5a5eabd8
VeneLooool <timofej.gromb38@gmail.com>
2022-05-09 14:33:19 +0300

    last 5 tasks done

--- shorthash/shorthash.go
+++ shorthash/shorthash.go
@@ -1,5 +1,16 @@
 package shorthash
 
-func GenerateShortHashes(dictionary string, len int) []string {
-	return []string{}
+func GenerateShortHashes(dictionary string, curLen int) (result []string) {
+	if curLen == 0 || dictionary == "" {
+		return []string{}
+	}
+
+	for _, v := range dictionary {
+		result = append(result, string(v))
+		for _, value := range GenerateShortHashes(dictionary, curLen-1) {
+			result = append(result, string(v)+value)
+		}
+	}
+
+	return result
 }
--- warriors/warriors.go
+++ warriors/warriors.go
@@ -1,5 +1,57 @@
 package warriors
 
+import "strings"
+
 func Count(image string) int {
-	return 0
+	if image == "" {
+		return 0
+	}
+
+	counter := 1
+	newImage := parseImage(image)
+
+	for i, line := range newImage {
+		for j, column := range line {
+			if column > 1 {
+				paintRegion(column, i, j, newImage)
+			}
+			if column == 1 {
+				counter++
+				paintRegion(counter, i, j, newImage)
+			}
+		}
+	}
+	return counter - 1
+}
+
+func parseImage(image string) (parsedResult [][]int) {
+
+	rows := strings.Split(image, "\n")
+	parsedResult = make([][]int, len(rows)+1)
+	for curRow, line := range rows {
+		parsedResult[curRow] = make([]int, len(line))
+		for column, thisRune := range line {
+			parsedResult[curRow][column] = int(thisRune - '0')
+		}
+	}
+	return parsedResult
+
+}
+
+func paintRegion(color, pointI, pointJ int, image [][]int) {
+	for i := 0; i <= 1; i++ {
+		for j := -i; j <= 1; j++ {
+			toPaintI := pointI + i
+			toPaintJ := pointJ + j
+
+			isInBorder := false
+			if toPaintJ >= 0 && toPaintI < len(image) && toPaintJ < len(image[toPaintI]) {
+				isInBorder = true
+			}
+
+			if isInBorder && image[toPaintI][toPaintJ] == 1 {
+				image[toPaintI][toPaintJ] = color
+			}
+		}
+	}
 }
