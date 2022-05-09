33f3d79ad2d6d90dad3a1aef4733769e206e195f
VeneLooool <timofej.gromb38@gmail.com>
2022-05-09 14:30:25 +0300

    another 5 tasks done

--- mergesort/mergesort.go
+++ mergesort/mergesort.go
@@ -2,5 +2,28 @@ package mergesort
 
 // MergeSort is used to sort an array of integer
 func MergeSort(input []int) []int {
-	return []int{}
+	lengthInput := len(input)
+	if lengthInput <= 1 {
+		return input
+	}
+	firstHalf := MergeSort(input[:lengthInput/2])
+	secondHalf := MergeSort(input[lengthInput/2:])
+	var firstPointer, secondPointer, outPointer int
+	output := make([]int, lengthInput)
+	for firstPointer < len(firstHalf) {
+		for secondPointer < len(secondHalf) && secondHalf[secondPointer] < firstHalf[firstPointer] {
+			output[outPointer] = secondHalf[secondPointer]
+			secondPointer++
+			outPointer++
+		}
+		output[outPointer] = firstHalf[firstPointer]
+		firstPointer++
+		outPointer++
+	}
+	for secondPointer < len(secondHalf) {
+		output[outPointer] = secondHalf[secondPointer]
+		secondPointer++
+		outPointer++
+	}
+	return output
 }
